254c94ca283cd72243694d9ff5749acbda0da136
Sergey Ponomarev <stokito@gmail.com>
2023-05-02 21:01:33 +0300

    file_parse_test.go ctx

--- file_parse_test.go
+++ file_parse_test.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"context"
 	"github.com/stretchr/testify/assert"
 	"io"
 	"testing"
 )
 
 func Test_ParsePortsStream(t *testing.T) {
+	ctx := context.Background()
 	var portsFile io.Reader
 	res := make(chan ParseStream, 4)
 
-	go ParsePortsStream(portsFile, res)
+	go ParsePortsStream(ctx, portsFile, res)
 	r1 := <-res
 	r2 := <-res
 	assert.Equal(t, "1", r1.Value.Unlocs[0])
