79eabeac94a5ddcd40c6ec3e14acbe0663dac5f1
Sergey Ponomarev <stokito@gmail.com>
2023-05-03 11:02:15 +0300

    Implement streaming parser

--- file_parse.go
+++ file_parse.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"log"
 	"os"
 )
+
+var ErrorBadUnlock = errors.New("bad Unlock")
 
 func ParsePortsFile(ctx context.Context, portsFilePath string) error {
 	portsFile, err := os.Open(portsFilePath)
@@ -48,21 +51,37 @@ type Port struct {
 // A ParseStream is used to stream back results.
 // Either Error or Value will be set on returned results.
 type ParseStream struct {
-	Value *Port
-	Error error
+	// Unlock code, can be empty on parsing error
+	Unlock string
+	Value  *Port
+	Error  error
 }
 
 func ParsePortsStream(ctx context.Context, portsFile io.Reader, res chan<- ParseStream) {
 	defer close(res)
-	port := &Port{
-		Unlocs: []string{"1"},
-	}
-	res <- ParseStream{
-		Value: port,
-		Error: nil,
-	}
-	res <- ParseStream{
-		Value: port,
-		Error: errors.New("Error"),
+
+	jsonDec := json.NewDecoder(portsFile)
+	_, _ = jsonDec.Token() // skip first {
+	for jsonDec.More() {
+		unlockToken, err := jsonDec.Token()
+		if err != nil {
+			res <- ParseStream{Error: err}
+			continue
+		}
+		unlock, ok := unlockToken.(string)
+		if !ok {
+			res <- ParseStream{Error: ErrorBadUnlock}
+			continue
+		}
+		//TODO Get the raw JSON of the Port instead of mapping to the struct.
+		// The port structure is not fully specified and can contain many other fields.
+		// The Golang's decoder doesn't support getting of the raw json in effective way.
+		port := &Port{}
+		err = jsonDec.Decode(port)
+		if err != nil {
+			res <- ParseStream{Unlock: unlock, Error: err}
+			continue
+		}
+		res <- ParseStream{Unlock: unlock, Value: port}
 	}
 }
--- file_parse_test.go
+++ file_parse_test.go
@@ -3,18 +3,64 @@ package main
 import (
 	"context"
 	"github.com/stretchr/testify/assert"
-	"io"
+	"strings"
 	"testing"
 )
 
+// language=JSON
+var testPortsJson = `
+{
+  "DJJIB": {
+    "name": "Djibouti",
+    "city": "Djibouti",
+    "country": "Djibouti",
+    "alias": [],
+    "regions": [],
+    "coordinates": [
+      43.1456475,
+      11.5720765
+    ],
+    "province": "Djibouti",
+    "timezone": "Africa/Djibouti",
+    "unlocs": [
+      "DJJIB",
+      "DJPOD"
+    ],
+    "code": "77701"
+  },
+  "DJPOD": {
+    "name": "Djibouti",
+    "city": "Djibouti",
+    "country": "Djibouti",
+    "alias": [],
+    "regions": [],
+    "coordinates": [
+      43.1456475,
+      11.5720765
+    ],
+    "province": "Djibouti",
+    "timezone": "Africa/Djibouti",
+    "unlocs": [
+      "DJPOD",
+      "DJJIB"
+    ],
+    "code": "77701"
+  },
+  "ERR": {
+    "name": "ERROR Unclosed quote
+  }
+}`
+
 func Test_ParsePortsStream(t *testing.T) {
 	ctx := context.Background()
-	var portsFile io.Reader
-	res := make(chan ParseStream, 4)
+	portsFile := strings.NewReader(testPortsJson)
+	res := make(chan ParseStream, 0)
 
 	go ParsePortsStream(ctx, portsFile, res)
 	r1 := <-res
+	assert.Equal(t, []string{"DJJIB", "DJPOD"}, r1.Value.Unlocs)
 	r2 := <-res
-	assert.Equal(t, "1", r1.Value.Unlocs[0])
-	assert.NotNil(t, r2.Error)
+	assert.Equal(t, []string{"DJPOD", "DJJIB"}, r2.Value.Unlocs)
+	r3 := <-res
+	assert.NotNil(t, r3.Error)
 }
