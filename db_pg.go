b9b939a9f882bd9e829daf59a5fb9324e609595e
Sergey Ponomarev <stokito@gmail.com>
2023-05-03 21:10:33 +0300

    PostgresDb: implement FindPort(), GetAll() and add a test

--- db_pg.go
+++ db_pg.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"log"
+	"sort"
 )
 
 // language=PostgreSQL
-var sqlUpsertPort = `INSERT INTO ports (
-	unlocs, port)
-	VALUES ($1, $2)
+var sqlUpsertPort = `
+INSERT INTO ports (
+unlocs, port, ids)
+VALUES ($1, $2, $3)
+`
+
+// language=PostgreSQL
+var sqlGetPort = `
+SELECT unlocs, port, ids
+FROM ports 
+WHERE $1 = ANY(ids)
 `
 
 type PostgresDb struct {
@@ -20,11 +31,6 @@ type PostgresDb struct {
 func NewPostgresDb(databaseUrl string) *PostgresDb {
 	return &PostgresDb{databaseUrl: databaseUrl}
 }
-
-//Connect(ctx context.Context) error
-//Close(ctx context.Context)
-//UpsertPort(ctx context.Context, portUnloc string, port *Port) error
-//FindPort(ctx context.Context, portUnloc string) *Port
 
 func (db *PostgresDb) Connect(ctx context.Context) error {
 	poolConfig, err := pgxpool.ParseConfig(db.databaseUrl)
@@ -49,19 +55,69 @@ func (db *PostgresDb) Close() {
 }
 
 func (db *PostgresDb) UpsertPort(ctx context.Context, newUnloc string, port *Port) {
+	sort.Strings(port.Unlocs)
 	_, sqlErr := db.pool.Exec(ctx, sqlUpsertPort,
-		newUnloc, port)
+		newUnloc, port, port.Unlocs)
 	if sqlErr != nil {
 		log.Printf("WARN Fail to upsert port %v\n", sqlErr)
 	}
 }
 
 func (db *PostgresDb) FindPort(ctx context.Context, newUnloc string) *Port {
-	//TODO
-	return nil
+	rows, err := db.pool.Query(ctx, sqlGetPort, newUnloc)
+	if err != nil {
+		log.Printf("ERR GetAll error: %s\n", err)
+		return nil
+	}
+	defer rows.Close()
+	if !rows.Next() {
+		return nil
+	}
+	port := db.scanRow(rows)
+	return port
 }
 
 func (db *PostgresDb) GetAll(ctx context.Context) []*Port {
-	//TODO
-	return nil
+	rows, err := db.pool.Query(ctx, "SELECT unlocs, port, ids FROM ports")
+	if err != nil {
+		log.Printf("ERR GetAll error: %s\n", err)
+		return []*Port{}
+	}
+	defer rows.Close()
+	allPorts := []*Port{}
+	for rows.Next() {
+		port := db.scanRow(rows)
+		if port == nil {
+			continue
+		}
+		allPorts = append(allPorts, port)
+	}
+	return allPorts
+}
+
+func (db *PostgresDb) scanRow(rows pgx.Rows) *Port {
+	var unlocs string
+	var portJson string
+	var ids []string
+	scanErr := rows.Scan(&unlocs, &portJson, &ids)
+	if scanErr != nil {
+		log.Printf("ERR scan error: %s\n", scanErr)
+		return nil
+	}
+	port := &Port{}
+	jsonErr := json.Unmarshal([]byte(portJson), port)
+	if jsonErr != nil {
+		log.Printf("ERR scan error: %s\n", jsonErr)
+		return nil
+	}
+	return port
+}
+
+func (db *PostgresDb) RemoveAll() {
+	ctx := context.Background()
+	_, err := db.pool.Exec(ctx, "DELETE FROM ports")
+	if err != nil {
+		log.Printf("ERR GetAll error: %s\n", err)
+		return
+	}
 }
---
+++ db_pg_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func Test_PostgresDb(t *testing.T) {
+	ctx := context.Background()
+	db := NewPostgresDb(os.Getenv("DATABASE_URL"))
+	err := db.Connect(ctx)
+	assert.NoError(t, err)
+	defer db.Close()
+	db.RemoveAll()
+	defer db.RemoveAll()
+	portToSave := &Port{
+		Name:   "1",
+		Unlocs: []string{"1", "2"},
+	}
+	portToSave2 := &Port{
+		Name:   "2",
+		Unlocs: []string{"1", "2"},
+	}
+	db.UpsertPort(ctx, "1", portToSave)
+	storedPort := db.FindPort(ctx, "1")
+	assert.Equal(t, "1", storedPort.Name)
+	assert.Equal(t, "1", storedPort.Unlocs[0])
+	assert.Equal(t, "2", storedPort.Unlocs[1])
+	allPorts := db.GetAll(ctx)
+	assert.Equal(t, "1", allPorts[0].Name)
+	assert.Equal(t, "1", allPorts[0].Unlocs[0])
+	assert.Equal(t, "2", allPorts[0].Unlocs[1])
+	assert.Equal(t, 1, len(allPorts))
+
+	db.UpsertPort(ctx, "2", portToSave2)
+	storedPort = db.FindPort(ctx, "2")
+	assert.Equal(t, "2", storedPort.Name)
+	assert.Equal(t, "1", storedPort.Unlocs[0])
+	assert.Equal(t, "2", storedPort.Unlocs[1])
+	allPorts = db.GetAll(ctx)
+	assert.Equal(t, "2", allPorts[0].Name)
+	assert.Equal(t, "1", allPorts[0].Unlocs[0])
+	assert.Equal(t, "2", allPorts[0].Unlocs[1])
+	assert.Equal(t, 1, len(allPorts))
+}
