144f2eb216ba9ec2ebeb3adeecfd8e8b084d8baf
Sergey Ponomarev <stokito@gmail.com>
2023-05-03 22:20:21 +0300

    Load configuration from conf.json

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"os"
 )
 
 type PortsServiceConf struct {
@@ -13,7 +15,7 @@ type PortsServiceConf struct {
 var conf *PortsServiceConf
 
 func main() {
-	conf = &PortsServiceConf{DatabaseUrl: "postgres://postgres:postgres@127.0.0.1:5432/portsdb?search_path=ports_schema"}
+	loadConfig()
 	log.Printf("Start Ports Service\n")
 	ctx := context.Background()
 	dbInitErr := InitDb()
@@ -33,4 +35,30 @@ func main() {
 		return
 	}
 	fmt.Println("Total Processed ", totalProcessed)
+}
+
+// loadConfig Load settings from conf.json and optionally from conf.local.json (used for a local development)
+func loadConfig() {
+	conf = &PortsServiceConf{}
+	confBytes, err := os.ReadFile("conf.json")
+	if err != nil {
+		log.Fatalf("Unable to read conf.json: %s\n", err)
+	}
+	err = json.Unmarshal(confBytes, conf)
+	if err != nil {
+		log.Fatalf("Unable to read conf.json broken JSON: %s\n", err)
+	}
+	// override config fields from conf.local.json
+	confBytes, err = os.ReadFile("conf.local.json")
+	if err != nil {
+		if os.IsNotExist(err) {
+			return
+		}
+		log.Fatalf("Unable to read conf.local.json: %s\n", err)
+	}
+	err = json.Unmarshal(confBytes, conf)
+	if err != nil {
+		log.Fatalf("Unable to read conf.json broken JSON: %s\n", err)
+	}
+	log.Printf("INFO config overriden from conf.local.json")
 }
